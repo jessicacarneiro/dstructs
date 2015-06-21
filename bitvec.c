458e7c4f709fb907e6f5d36410de52d7cc5463fc
Italo Cunha <cunha@dcc.ufmg.br>
2015-06-20 23:19:40 -0300

    add bitvec

---
+++ bitvec.c
@@ -0,0 +1,36 @@
+#include <assert.h>
+
+#include "bitvec.h"
+#include "log.h"
+
+struct bitvec * bitvec_create(size_t bits)
+{
+	assert(sizeof(unsigned) == 4);
+	struct bitvec *vec = malloc(sizeof(*vec));
+	if(!vec) logea(__FILE__, __LINE__, NULL);
+	vec->nwords = (bits >> bitvec_word_logbits);
+	vec->nwords += (bits & (bitvec_word_bits - 1)) ? 1 : 0;
+	vec->nbits = bits;
+	vec->v = calloc(vec->nwords, sizeof(unsigned));
+	if(!vec->v) logea(__FILE__, __LINE__, NULL);
+	return vec;
+}
+
+void bitvec_destroy(struct bitvec *vec)
+{
+	free(vec->v);
+	free(vec);
+}
+
+void bitvec_iterate(struct bitvec *vec, int (*cb)(size_t, void *), void *data)
+{
+	for(size_t w = 0; w < vec->nwords; w++) {
+		unsigned word = vec->v[w];
+		size_t base = w << bitvec_word_logbits;
+		while(word) {
+			int i = __builtin_ffs(word) - 1;
+			if(0 != cb(base + i, data)) return;
+			word ^= 1 << i;
+		}
+	}
+}
---
+++ bitvec.h
@@ -0,0 +1,45 @@
+#ifndef __BITVECTOR_HEADER__
+#define __BITVECTOR_HEADER__
+
+#include <stdlib.h>
+
+struct bitvec {
+	unsigned *v;
+	size_t nbits;
+	size_t nwords;
+};
+
+struct bitvec * bitvec_create(size_t bits);
+void bitvec_destroy(struct bitvec *vec);
+
+/* REQUIRES sizeof(unsigned) == 4 */
+#define bitvec_word_bits 32
+#define bitvec_word_logbits 5
+inline void bitvec_set(struct bitvec *vec, size_t i)
+{
+	size_t w = i >> bitvec_word_logbits;
+	unsigned mask = 1 << (i & (bitvec_word_bits - 1));
+	vec->v[w] |= mask;
+}
+inline unsigned bitvec_test(struct bitvec *vec, size_t i)
+{
+	size_t w = i >> bitvec_word_logbits;
+	unsigned mask = 1 << (i & (bitvec_word_bits - 1));
+	return vec->v[w] & mask;
+}
+inline unsigned bitvec_test_set(struct bitvec *vec, size_t i)
+{
+	size_t w = i >> bitvec_word_logbits;
+	unsigned mask = 1 << (i & (bitvec_word_bits - 1));
+	unsigned ret = vec->v[w] & mask;
+	vec->v[w] |= mask;
+	return ret;
+}
+
+/* This function calls `cb` for each bit set in `vec`.  It returns
+ * when `cb` returns nonzero or when all bits in `vec` have been
+ * passed to `cb`.  Pointer `data` is passed as the second parameter
+ * to `cb`. */
+void bitvec_iterate(struct bitvec *vec, int (*cb)(size_t, void *), void *data);
+
+#endif

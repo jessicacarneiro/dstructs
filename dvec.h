1077060a5319fc51a08fe4c937cbed9c280423b7
Italo Cunha <cunha@dcc.ufmg.br>
2016-11-28 03:42:54 -0200

    update dvec

--- dvec.h
+++ dvec.h
@@ -18,28 +18,33 @@
  * }
  **/
 
+#define dvec_declare_struct(name, type) struct name { size_t sz; size_t max; type *data; }
+
 #define dvec_struct(type) struct { size_t sz; size_t max; type *data; }
 
-#define dvec_init(vector, initsz) { \
+#define dvec_init(_v1, initsz) { \
 	assert(initsz > 0); \
-	vector.sz = 0; vector.max = initsz; \
-	vector.data = malloc(vector.max * sizeof(vector.data[0])); \
-	if(!vector.data) logea(__FILE__, __LINE__, NULL); \
+	(_v1).sz = 0; \
+	(_v1).max = initsz; \
+	(_v1).data = malloc((_v1).max * sizeof((_v1).data[0])); \
+	if(!(_v1).data) logea(__FILE__, __LINE__, NULL); \
 }
 
-#define dvec_append(vector, value) { \
-	vector.data[vector.sz] = value; \
-	vector.sz++; \
-	dvec_check_expand(vector); \
+#define dvec_append(_v1, value) { \
+	(_v1).data[(_v1).sz] = value; \
+	(_v1).sz++; \
+	dvec_check_expand(_v1); \
 }
 
-#define dvec_check_expand(vector) { \
-	if(vector.sz == vector.max) { \
-		vector.max << 1; \
-		vector.data = realloc(vector.data, \
-				vector.max * sizeof(vector.data[0])); \
-		if(!vector.data) logea(__FILE__, __LINE__, NULL); \
+#define dvec_check_expand(_v1) \
+	if((_v1).sz == (_v1).max) { \
+		(_v1).max <<= 1; \
+		(_v1).data = realloc((_v1).data, \
+				(_v1).max * sizeof((_v1).data[0])); \
+		if(!(_v1).data) logea(__FILE__, __LINE__, NULL); \
 	} \
-}
+
+#define dvec_memcmp(_v1, _v2) ((_v1).sz != ((_v2).sz) ? (_v2).sz - (_v1).sz : \
+	memcmp((_v1).data, (_v2).data, (_v1).sz * sizeof((_v1).data[0])))
 
 #endif
